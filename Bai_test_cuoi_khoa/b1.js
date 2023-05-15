3bdbfdd5ad8f98bf2bdd66e14ae813f736e578ba
TranHuy98 <huy8663@gmail.com>
2023-05-15 23:19:16 +0700

    Bai test cuoi khoa

--- Bai_test_cuoi_khoa/b1.js
+++ Bai_test_cuoi_khoa/b1.js
@@ -1,37 +1,37 @@
 let execute = document.getElementById("execute");
 
-function primeCheck(n){
+function primeCheck(n) {
     let isPrime = 0;
-    for (let i = 1; i<=n; i++){
-        if(n%i === 0){
-            isPrime+=1;
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            isPrime += 1;
         }
     }
-    if (isPrime===2){
+    if (isPrime === 2) {
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
 
-execute.onclick = function (){
+execute.onclick = function () {
 
     let numberA = Number(document.getElementById("numberA").value);
     let numberB = Number(document.getElementById("numberB").value);
-   
+
     let prideNum = "";
     let primeSum = 0;
 
-    if(numberA%1==0 && numberB%1==0 && numberA<numberB){
-        for(let i = numberA; i<=numberB; i++){
-            if (primeCheck(i)){
-                primeSum+=i;
+    if (numberA % 1 == 0 && numberB % 1 == 0 && numberA < numberB) {
+        for (let i = numberA; i <= numberB; i++) {
+            if (primeCheck(i)) {
+                primeSum += i;
             }
         }
         document.getElementById("sum").innerHTML = `Tổng của dãy các số nguyên tố từ ${numberA} đến ${numberB} là: ${primeSum}`;
     }
-    else{
+    else {
         alert(`Dữ liệu nhập vào chưa đúng. Hãy nhập 2 số nguyên a và b với a<b`);
         document.getElementById("numberA").value = ``;
         document.getElementById("numberB").value = ``;
--- Bai_test_cuoi_khoa/b2.js
+++ Bai_test_cuoi_khoa/b2.js
@@ -1,27 +1,27 @@
 let execute = document.getElementById("execute");
 
-execute.onclick = function(){
+execute.onclick = function () {
     let number = document.getElementById("number").value;
     console.log(number);
     let kq = "";
     let line = "";
 
-    if(number%1==0 && number>1 && number<11){
-        for(let i=1; i<=number;i++){
-            for(let j=1; j<=i; j++){
+    if (number % 1 == 0 && number > 1 && number < 11) {
+        for (let i = 1; i <= number; i++) {
+            for (let j = 1; j <= i; j++) {
                 line += "*";
             }
-            kq+=`${line}</br>`;
+            kq += `${line}</br>`;
             line = ``;
         }
     }
-    else if(number == 1){
+    else if (number == 1) {
         kq = `*`;
     }
-    else{
+    else {
         alert('Dữ liệu nhập chưa đúng, hãy nhập số nguyên trong khoảng từ 1 đến 10');
         document.getElementById("number").value = ``;
     }
-    
+
     document.getElementById("kq").innerHTML = kq;
 }
